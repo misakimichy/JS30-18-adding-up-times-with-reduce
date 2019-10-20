a426ee6a49e286b7a8c76c81d2814db222d5cfa1
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-20 14:27:11 -0700

    Render each video length and total video length.

--- js/main.js
+++ js/main.js
@@ -4,12 +4,21 @@
         let [minutes, seconds] =  current.dataset.time.split(':').map(parseFloat);
         return total += minutes * 60 + seconds;
     }, 0);
-
+    
     // Convert this into hh:mm:ss format
     const hour = seconds / 3600 <= 1 ? 0 : Math.floor(seconds / 3600);
     const min = Math.floor((seconds % 3600) / 60);
     const sec = (seconds % 3600) % 60;
     
-    const showTotal = `<span>${hour}:${min}:${sec}</span>`;
-    document.querySelector('#total').innerHTML = `Total length of those videos: ${showTotal}`;
+    // Add each video length
+    const videoLength = timeNodes.map(timeNode => {
+        return `
+            <li>${timeNode.dataset.time}</li>
+        `;
+    }).join('');
+    document.querySelector('#video-length').innerHTML = videoLength;
+   
+    // Add total video length
+    const showTotal = `<span id="length">${hour}:${min}:${sec}</span>`;
+    document.querySelector('#total').innerHTML = `Total length of videos: ${showTotal}`;
 }());
