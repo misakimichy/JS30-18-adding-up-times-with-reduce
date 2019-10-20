e295518204e9d5bafdc2b401d7d3637e98d32208
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-20 13:45:58 -0700

    Render the total length of the videos to the HTML.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 (function(){
     const timeNodes = [...document.querySelectorAll('[data-time]')];
-    const seconds = timeNodes
-        .map(node => node.dataset.time)
-        .map(timeCode => {
-            const [minutes, seconds] = timeCode.split(':').map(parseFloat);
-            return (minutes * 60) + seconds;
-        }).reduce((total, videoSeconds) => total + videoSeconds);
-    let secondsLeft = seconds;
-    const hours = Math.floor(secondsLeft / 3600);
-    secondsLeft = secondsLeft % 3600;
-    const minutes = Math.floor(secondsLeft / 60);
-    secondsLeft = secondsLeft % 60;
-    console.log(hours, minutes, secondsLeft);
+    let seconds = timeNodes.reduce((total, current, index) => {
+        let [minutes, seconds] =  current.dataset.time.split(':').map(parseFloat);
+        return total += minutes * 60 + seconds;
+    }, 0);
+
+    // Convert this into hh:mm:ss format
+    const hour = seconds / 3600 <= 1 ? 0 : Math.floor(seconds / 3600);
+    const min = Math.floor((seconds % 3600) / 60);
+    const sec = (seconds % 3600) % 60;
+    
+    const showTotal = `<span>${hour}:${min}:${sec}</span>`;
+    document.querySelector('#total').innerHTML = `Total length of those videos: ${showTotal}`;
 }());
