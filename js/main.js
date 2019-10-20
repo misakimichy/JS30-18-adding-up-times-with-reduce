9fbe6808b49c8d0afd7358b3b4a94a2bbd14070b
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-20 13:18:10 -0700

    Calculate hours, minutes and seconds.

--- js/main.js
+++ js/main.js
@@ -6,5 +6,10 @@
             const [minutes, seconds] = timeCode.split(':').map(parseFloat);
             return (minutes * 60) + seconds;
         }).reduce((total, videoSeconds) => total + videoSeconds);
-    console.log(seconds);
+    let secondsLeft = seconds;
+    const hours = Math.floor(secondsLeft / 3600);
+    secondsLeft = secondsLeft % 3600;
+    const minutes = Math.floor(secondsLeft / 60);
+    secondsLeft = secondsLeft % 60;
+    console.log(hours, minutes, secondsLeft);
 }());
